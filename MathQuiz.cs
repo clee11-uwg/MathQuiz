7159bb674d438772362509b1099b4def4877f63f
leecodya <lee.codya@gmail.com>
2021-01-15 13:05:56 -0500

    added CheckTheAnswer method to check the answer to see if the user got everything right

--- MathQuiz.cs
+++ MathQuiz.cs
@@ -61,6 +61,18 @@ namespace MathQuiz
             timer1.Start();
         }
 
+        /// <summary>
+        /// Check the answer to see if th euser got everything right
+        /// </summary>
+        /// <returns>True if the answer's correct, false otherwise</returns>
+        private bool CheckTheAnswer()
+        {
+            if (addend1 + addend2 == sum.Value)
+                return true;
+            else
+                return false;
+        }
+
         private void StartButton_Click(object sender, EventArgs e)
         {
             StartTheQuiz();
